8a6a5c14cec5f464295e218d0e0bd0b43337eabe
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2024-11-13 23:33:43 +0530

    commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { CardHoverEffectDemo } from "@/components/Cards";
 import { GoogleGeminiEffectDemo } from "@/components/GeminiEffect";
 import { HeroSection } from "@/components/HeroSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
