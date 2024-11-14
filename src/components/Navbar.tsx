a7a78e3fde7af9897d9e621e2d39d888ba7f6234
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2024-11-15 00:06:31 +0530

    ButtonFixNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,31 +10,32 @@ function Navbar({ className }: { className?: string }) {
         className={cn("fixed top-5 inset-x-0 max-w-2xl mx-auto z-50", className)}
       >
         <Menu setActive={setActive}>
-          <MenuItem setActive={setActive} active={active} item="Services">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="#">Web Development</HoveredLink>
-              <HoveredLink href="#">Interface Design</HoveredLink>
-              <HoveredLink href="#">Search Engine Optimization</HoveredLink>
-              <HoveredLink href="#">Branding</HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="#">Web Development</HoveredLink>
-              <HoveredLink href="#">Interface Design</HoveredLink>
-              <HoveredLink href="#">Search Engine Optimization</HoveredLink>
-              <HoveredLink href="#">Branding</HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Pricing">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="#">Hobby</HoveredLink>
-              <HoveredLink href="#">Individual</HoveredLink>
-              <HoveredLink href="#">Team</HoveredLink>
-              <HoveredLink href="#">Enterprise</HoveredLink>
-            </div>
-          </MenuItem>
-        </Menu>
+  <MenuItem setActive={setActive} active={active} item="Chatrooms">
+    <div className="flex flex-col space-y-4 text-sm">
+      <HoveredLink href="#">Public Chatrooms</HoveredLink>
+      <HoveredLink href="#">Private Chatrooms</HoveredLink>
+      <HoveredLink href="#">Popular Chatrooms</HoveredLink>
+      <HoveredLink href="#">Create a Chatroom</HoveredLink>
+    </div>
+  </MenuItem>
+  <MenuItem setActive={setActive} active={active} item="Community">
+    <div className="flex flex-col space-y-4 text-sm">
+      <HoveredLink href="#">Community Rules</HoveredLink>
+      <HoveredLink href="#">Guidelines</HoveredLink>
+      <HoveredLink href="#">Support</HoveredLink>
+      <HoveredLink href="#">FAQ</HoveredLink>
+    </div>
+  </MenuItem>
+  <MenuItem setActive={setActive} active={active} item="Account">
+    <div className="flex flex-col space-y-4 text-sm">
+      <HoveredLink href="#">Profile</HoveredLink>
+      <HoveredLink href="#">Settings</HoveredLink>
+      <HoveredLink href="#">Notifications</HoveredLink>
+      <HoveredLink href="#">Logout</HoveredLink>
+    </div>
+  </MenuItem>
+</Menu>
+
       </div>
     );
   }
